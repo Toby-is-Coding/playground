4b0a25567cecde5e1ccaa29013ac76f81ab55458
Toby-is-duck <77500195+Toby-is-duck@users.noreply.github.com>
2021-03-06 15:34:51 +0200

    Update hangman.cpp

--- hangman.cpp
+++ hangman.cpp
@@ -17,17 +17,17 @@ string wrongGuesses;
 
 void startGame()
 {
-	cout <<"Let's play hangman! What's the word?" <<endl;
+	cout << "Let's play hangman! What's the word?" << endl;
 	cin >> word; //Getting the word you said
 	wordDisplay = word;
-	for (int i = 0; i<word.length(); i++) { //Creating wordDisplay to have the same amount of -'s as the word length
+	for (int i = 0; i < word.length(); i++) { //Creating wordDisplay to have the same amount of -'s as the word length
 		wordDisplay[i] = '-';
 	}
-	cout <<"How many attempts does Player 2 have to guess the word?" <<endl;
+	cout << "How many attempts does Player 2 have to guess the word?" << endl;
 	cin >> attempts; //Getting the amount of attempts you said
-	cout <<"Clearing chat... \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" //Clearing Chat
-	<<"Chat cleared." <<endl;
-	cout <<wordDisplay <<endl; //Displaying hidden word (----)
+	system("cls"); //Clearing Chat
+	cout << "Chat cleared." << endl;
+	cout << wordDisplay << endl; //Displaying hidden word (----)
 	return;
 }
 
@@ -37,43 +37,43 @@ int main()
 	string theWordAttempt;
 	while (hangingMan == true) {
 		startGame(); //Calling the startGame function
-		cout <<"The word has " <<word.length() <<" characters." <<endl;
+		cout << "The word has " << word.length() << " characters." << endl;
 		if (attempts == 1) { //Grammar correcting
-			cout <<"You have " <<attempts <<" attempt to guess the word." <<endl;
+			cout << "You have " << attempts << " attempt to guess the word." << endl;
 		}
 		else {
-			cout <<"You have " <<attempts <<" attempts to guess the word." <<endl;
+			cout << "You have " << attempts << " attempts to guess the word." << endl;
 		}
-		cout <<"You can start guessing now." <<endl;
+		cout << "You can start guessing now." << endl;
 		while (attempts > 0 && word != wordDisplay) { //while attempts > 0 AND word does not equal to displayed hidden word, continue guessing
 			guessing(); //Calling the guessing function
 			display(); //Calling the display function
 		}
 		if (attempts <= 0) {
-			cout <<"You ran out of attempts! You lost. \n The word was \"" <<word <<"\"" <<endl; //Lose message
+			cout << "You ran out of attempts! You lost. \n The word was \"" << word << "\"" << endl; //Lose message
 		}
-		else if (attempts > 0){
-			attempts>1? theWordAttempt = "attempts": theWordAttempt = "attempt"; //Grammar fixing
-			cout <<"You guessed the word \"" <<word <<"\" with only " <<attempts <<" " <<theWordAttempt <<" left! You won!" <<endl; //Win message
+		else if (attempts > 0) {
+			attempts > 1 ? theWordAttempt = "attempts" : theWordAttempt = "attempt"; //Grammar fixing
+			cout << "You guessed the word \"" << word << "\" with only " << attempts << " " << theWordAttempt << " left! You won!" << endl; //Win message
 		}
-		cout <<"Would you like to play again?" <<endl;
+		cout << "Would you like to play again?" << endl;
 		string playAgain;
 		bool understand = false;
 		while (understand == false) {
 			cin >> playAgain; //Getting your answer
-			switch(toupper(playAgain[0])) {
+			switch (toupper(playAgain[0])) {
 			case 'N': //if answer is no
-				cout <<"'Twas fun, bye!" <<endl;
+				cout << "'Twas fun, bye!" << endl;
 				hangingMan = false;
 				understand = true;
 				break;
 			case 'Y': //if answer is yes
-				cout <<endl <<"Alright. ";
+				cout << endl << "Alright. ";
 				wrongGuesses.clear(); //Clears the wrongGuesses variable from last game
 				understand = true;
 				break;
 			default:
-				cout <<"I didn't understand, please say yes or no." <<endl; //if answer is neither yes or no, ask again
+				cout << "I didn't understand, please say yes or no." << endl; //if answer is neither yes or no, ask again
 				break;
 			}
 		}
@@ -89,7 +89,7 @@ void guessing()
 	bool alreadyGuessed = false;
 	for (int i = 0; i < word.length(); i++) {
 		if (playerGuess == wordDisplay[i] && alreadyWarned == false) { //Checks if playerGuess is already in wordDisplay & accounts for double warning
-			cout <<"You already guessed that letter!" <<endl;
+			cout << "You already guessed that letter!" << endl;
 			alreadyWarned = true;
 			alreadyGuessed = true;
 		}
@@ -100,17 +100,17 @@ void guessing()
 	}
 	for (int j = 0; j < wrongGuesses.length(); j++) {
 		if (playerGuess == wrongGuesses[j]) { //Checks if playerGuess is already in wrongGuesses
-			cout <<"You already guessed that letter!" <<endl;
+			cout << "You already guessed that letter!" << endl;
 			alreadyGuessed = true;
 		}
 	}
 	if (letterFound == false && alreadyGuessed == false) { //if guessed letter isn't in word, add it to wrongGuesses
 		wrongGuesses += playerGuess;
 		attempts--; //use up an attempt if guessed incorrectly
-		cout <<"You guessed wrong!" <<endl;
+		cout << "You guessed wrong!" << endl;
 	}
 	else if (letterFound == true && alreadyGuessed == false) {
-		cout <<"You guessed right!" <<endl;
+		cout << "You guessed right!" << endl;
 	}
 	return;
 }
@@ -121,15 +121,15 @@ void display()
 	if (word == wordDisplay || attempts <= 0) {
 		endGame = true;
 	}
-	cout <<endl <<wordDisplay <<endl; //Displaying wordDisplay
+	cout << endl << wordDisplay << endl; //Displaying wordDisplay
 	if (attempts == 1 && endGame == false) { //if there's only 1 attempt left, say attempt
-		cout <<"You have: " <<attempts <<" attempt remaining." <<endl;
+		cout << "You have: " << attempts << " attempt remaining." << endl;
 	}
 	else if (attempts != 1 && endGame == false) { //otherwise, say attempts
-		cout <<"You have: " <<attempts <<" attempts remaining." <<endl;
+		cout << "You have: " << attempts << " attempts remaining." << endl;
 	}
 	if (wrongGuesses.length() != 0) { //if there's nothing in wrongGuesses, don't say it
-		cout <<"Failed attempts: " <<wrongGuesses <<endl;
+		cout << "Failed attempts: " << wrongGuesses << endl;
 	}
 	return;
 }
